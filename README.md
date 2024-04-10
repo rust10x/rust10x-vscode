50f6c8fb452d2eee6dd971a24be0fe3eb4231ddb
Jeremy Chone <jeremy.chone@gmail.com>
2024-04-10 15:05:59 -0700

    + eol commands - first pass at adding the eol commands

--- README.md
+++ README.md
@@ -4,11 +4,34 @@ Rust10x is designed to enhance productivity in production coding with Rust.
 
 For more information and keybindings, visit: https://rust10x.com/vscode
 
-The initial release includes a collection of snippets that I frequently use in my daily Rust programming and in my [Rust Production Coding Videos](https://youtube.com/jeremychone).
+The initial releases include:
+1) A collection of snippets that I frequently use in my Rust programming and in my [Rust Production Coding Videos](https://youtube.com/jeremychone).
+2) Several Rust coding productivity commands. For now, these include the [`eol_..` commands](#2-commands).
+
+### 1) Snippets
 
 All snippets are prefixed with `10x-`, making them quick to select by just typing `1` and then triggering the suggestion. For example, typing `1tf` and then pressing `Ctrl+Space` will suggest `10x-test-fn`, `10x-test-async-fn`, and `10x-impl-try-from`. ([more info](https://rust10x.com/vscode))
 
-I haven't included keybindings as they are quite personal, but you can find the ones I use at [rust10x/vscode](https://rust10x.com/vscode).
+I haven't included keybindings as they are more of personal preferences personal, but you can find the ones I use at [rust10x/vscode](https://rust10x.com/vscode).
+
+### 2) Commands
+
+NEW in version 0.1.5 and later.
+
+The `eol_...` commands allow toggling the end-of-line completion. They will replace the existing end-of-line character with the one specified by the command.
+
+| Command                             | EOL        | Possible bindings |
+|-------------------------------------|------------|-------------------|
+| `rust10x.eol_scolon`                | `;`        | `cmd+;`           |
+| `rust10x.eol_question_scolon`       | `?;`       | `cmd+alt+;`       |
+| `rust10x.eol_await_scolon`          | `.await;`  | `cmd+ctrl+;`      |
+| `rust10x.eol_await_question_scolon` | `.await?;` | `cmd+ctrl+alt+;`  |
+
+
+> Note: I am showing the keybindings I use for these commands, but they are not set in this extension, as they might already be in use.
+> <br />In short, the pattern for these bindings is that `alt` adds the `?`, and `ctrl` adds the `.await`.
+
+You can find the my keybindings at [rust10x/vscode](https://rust10x.com/vscode).
 
 [GitHub Repo](https://github.com/rust10x/rust10x-vscode) | [VSCode Marketplace](https://marketplace.visualstudio.com/items?itemName=rust10x.rust10x)
 
---
+++ src/extension.ts
@@ -0,0 +1,143 @@
+import * as vscode from 'vscode';
+
+const ENDINGS = {
+	eol_scolon: ";",
+	eol_question_scolon: "?;",
+	eol_await_scolon: ".await;",
+	eol_await_question_scolon: ".await?;",
+}
+type EolType = keyof typeof ENDINGS;
+
+export function activate(context: vscode.ExtensionContext) {
+	// -- eol_scolon
+	let disposable = vscode.commands.registerCommand('rust10x.eol_scolon', make_eol_cmd_fn("eol_scolon"));
+	context.subscriptions.push(disposable);
+
+	// -- eol_question_scolon
+	disposable = vscode.commands.registerCommand('rust10x.eol_question_scolon', make_eol_cmd_fn("eol_question_scolon"));
+	context.subscriptions.push(disposable);
+
+	// -- eol_await_scolon
+	disposable = vscode.commands.registerCommand('rust10x.eol_await_scolon', make_eol_cmd_fn("eol_await_scolon"));
+	context.subscriptions.push(disposable);
+
+	// -- rust10x.eol_await_question_scolon
+	disposable = vscode.commands.registerCommand('rust10x.eol_await_question_scolon', make_eol_cmd_fn("eol_await_question_scolon"));
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
+type Fn = (...args: any[]) => any;
+
+function make_eol_cmd_fn(type: EolType): Fn {
+
+	function eol_scolon_applier(document: vscode.TextDocument, selection: vscode.Selection, edit: vscode.WorkspaceEdit) {
+		const line = document.lineAt(selection.active.line);
+		const lineText = line.text;
+		const match = findLongestMatch(lineText);
+		const ending = ENDINGS[type];
+
+		if (match == null) {
+			const position = line.range.end;
+			edit.insert(document.uri, position, ending);
+		} else if (match.value != ending) {
+			let newText = lineText.slice(0, -1 * match.value.length) + ending;
+			const lineRange = line.range;
+			edit.replace(document.uri, lineRange, newText);
+		}
+	}
+
+	return function () {
+		apply_for_each(eol_scolon_applier)
+	}
+
+}
+
+function eol_scolon_applier(document: vscode.TextDocument, selection: vscode.Selection, edit: vscode.WorkspaceEdit) {
+	const line = document.lineAt(selection.active.line);
+	const lineText = line.text;
+	const match = findLongestMatch(lineText);
+	if (match == null) {
+		const position = line.range.end;
+		edit.insert(document.uri, position, ';');
+	} else if (match.value != ";") {
+		let newText = lineText.slice(0, -1 * match.value.length) + ";";
+		const lineRange = line.range;
+		edit.replace(document.uri, lineRange, newText);
+	}
+}
+
+function eol_question_scolon_applier(document: vscode.TextDocument, selection: vscode.Selection, edit: vscode.WorkspaceEdit) {
+	const line = document.lineAt(selection.active.line);
+	if (!line.text.endsWith('?;')) {
+		const position = line.range.end;
+		edit.insert(document.uri, position, '?;');
+	}
+}
+
+function eol_await_question_scolon_applier(document: vscode.TextDocument, selection: vscode.Selection, edit: vscode.WorkspaceEdit) {
+	const line = document.lineAt(selection.active.line);
+	if (!line.text.endsWith('?;')) {
+		const position = line.range.end;
+		edit.insert(document.uri, position, '?;');
+	}
+}
+
+
+// #region    --- Support
+
+interface MatchResult {
+	value: string;
+	key: string;
+}
+
+function findLongestMatch(input: string): MatchResult | null {
+	let endings = ENDINGS;
+
+	let longestMatch: MatchResult | null = null;
+
+	Object.entries(endings).forEach(([key, value]) => {
+		if (input.endsWith(value)) {
+			// If a match is found and it is longer than the current longest match, update longestMatch
+			if (!longestMatch || value.length > longestMatch.value.length) {
+				longestMatch = { value: value, key };
+			}
+		}
+	});
+
+	return longestMatch;
+}
+
+
+type ApplierFn = (document: vscode.TextDocument, selection: vscode.Selection, edit: vscode.WorkspaceEdit) => void;
+
+function apply_for_each(applier: ApplierFn) {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showInformationMessage('No open text editor');
+		return;
+	}
+
+	const document = editor.document;
+	const selections = editor.selections;
+
+	const edit = new vscode.WorkspaceEdit();
+
+	selections.forEach((selection) => {
+		applier(document, selection, edit)
+	});
+	vscode.workspace.applyEdit(edit);
+
+	// now move selection to the end
+	const sels = editor.selections;
+	for (const sel of sels) {
+		const newLine = document.lineAt(sel.active.line);
+		const endOfLinePosition = new vscode.Position(sel.active.line, newLine.text.length);
+
+		editor.selection = new vscode.Selection(endOfLinePosition, endOfLinePosition);
+		editor.revealRange(new vscode.Range(endOfLinePosition, endOfLinePosition));
+	}
+
+}
+
+// #endregion --- Support
